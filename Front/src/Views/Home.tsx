e2dee7932512a874a865610d861e0ec13da399e2
Julien <julien.milants@gmail.com>
2024-10-31 22:00:34 +0100

    fix: fix de docker pour react

--- Front/src/Views/Home.tsx
+++ Front/src/Views/Home.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 const HomePage: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    console.log("ouais");
+    
+  };
   return (
    <>
     <Helmet>
         <title>Accueil - NapoPlate React</title>
     </Helmet>
-    <div className="text-center text-xl text-blue-800 font-bold">
+    <div className="text-center text-xl font-bold dark:text-white">
       Bienvenue sur la page d'accueil !
     </div>
+    <button onClick={toggleDarkMode} className='text-white'>
+        Changement de theme
+    </button>
    </>
   );
 };
